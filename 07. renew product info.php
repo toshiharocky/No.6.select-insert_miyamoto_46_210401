4b7522cb56bbcd0f376ae79e7504383c3a033f4c
toshiharocky <toshiharu69@gmail.com>
2021-03-29 14:35:22 +0900

    初期

---
+++ 07. renew product info.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>*******</title>
+    <!-- css -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="inventory.css">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<?php
+    // <!-- funcs.phpの読み込み -->
+        require_once("funcs.php");
+
+?>
+    
+</body>
+</html>
