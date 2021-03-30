64ea8ff5914fdec264e04022c74765d3d020621d
toshiharocky <toshiharu69@gmail.com>
2021-03-30 18:41:08 +0900

    情報更新機能の実装

---
+++ 07-1. choice category.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- css -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="inventory.css">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<form action="07-2. choice product name.php" method="post">
+    <button type="submit" name="category" value="supplements">supplements</button>
+    <button type="submit" name="category" value="clothes">clothes</button>
+    <button type="submit" name="category" value="equipment">equipment</button>
+    <button type="submit" name="category" value="books">books</button>
+</form>    
+
+</body>
+</html>
