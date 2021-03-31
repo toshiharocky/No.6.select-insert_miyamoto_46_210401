24ac728eab1b34a5017c80048b8ebc136716e8df
toshiharocky <toshiharu69@gmail.com>
2021-03-31 18:02:38 +0900

    移動機能の実装

---
+++ 08-1. move product category.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- css -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="inventory.css">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<h1>商品情報更新</h1>
+<h2>カテゴリーを選択してください</h2>
+<form action="08-2. choice move product name.php" method="post" style="text-align:center; margin-top:30px;">
+    <button class="btn" type="submit" name="category" value="supplements">supplements</button><br>
+    <button class="btn" type="submit" name="category" value="clothes">clothes</button><br>
+    <button class="btn" type="submit" name="category" value="equipment">equipment</button><br>
+    <button class="btn" type="submit" name="category" value="books">books</button>
+</form>
+
+</body>
+</html>
---
+++ 08-3. move product info.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>*******</title>
+    <!-- css -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="inventory.css">
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<?php
+// <!-- funcs.phpの読み込み -->
+        require_once("funcs.php");
+// 変数の受け取り
+    $model_num =$_POST['model_num'];
+// 商品名が表示されると、登録されている「商品番号」「在庫総数」「店舗内在庫」「倉庫内在庫」「納品待ち」「発注しきい値」が表示される
+    //1.  DB接続します
+    try {
+        //Password:MAMP='root',XAMPP=''
+        $pdo = new PDO('mysql:dbname=inventory_management;charset=utf8;host=localhost','root','root');
+    } catch (PDOException $e) {
+        exit('DBConnectError'.$e->getMessage());
+    }
+
+    //2．データ表示①（$table）
+    //２-1．データ取得SQL作成
+    $stmt = $pdo->prepare("SELECT * FROM total_db WHERE model_num = '$model_num'");
+    $status = $stmt->execute();
+
+    //2-2．データ表示
+    $table = "";
+    if ($status == false) {
+        //execute（SQL実行時にエラーがある場合）
+        $error = $stmt->errorInfo();
+        exit('ErrorQuery:' . print_r($error, true));
+    }else{
+        //Selectデータの数だけ自動でループしてくれる
+        //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+        while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $table .= 
+            "<tr>
+            <td>$result[model_num]</td>
+            <td>$result[product_name]</td>
+            </tr>";
+        }
+
+    }
+
+    //3．データ表示②（$info）
+    //3-1．データ取得SQL作成
+    $stmt = $pdo->prepare("SELECT * FROM total_db WHERE model_num = '$model_num'");
+    $status = $stmt->execute();
+
+    //3-2．データ表示②（$info）
+    $info = "";
+    if ($status == false) {
+        //execute（SQL実行時にエラーがある場合）
+        $error = $stmt->errorInfo();
+        exit('ErrorQuery:' . print_r($error, true));
+    }else{
+        //Selectデータの数だけ自動でループしてくれる
+        //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+        while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $info .= 
+            "
+            <table>
+                <tr>
+                    <td>在庫総数</td>
+                    <td>$result[total_amount]</td>
+                </tr>
+                <tr>
+                    <td>店舗内在庫</td>
+                    <td>$result[shop_amount]</td>
+                </tr>
+                <tr>
+                    <td>倉庫内在庫</td>
+                    <td>$result[warehouse_amount]</td>
+                </tr>
+            </table>
+            ";
+
+            $sent .= "<input type='hidden' name='total_amount' id='total_amount' value='$result[total_amount]'>
+            <input type='hidden' name='shop_amount' id='shop_amount' value='$result[shop_amount]'>
+            <input type='hidden' name='warehouse_amount' id='warehouse_amount' value='$result[warehouse_amount]'>
+            <input type='hidden' name='waiting_amount' id='waiting_amount' value='$result[waiting_amount]'>
+            <input type='hidden' name='threshold' id='threshold' value='$result[threshold]'>
+            <input type='hidden' name='category' id='category' value='$result[category]'>
+            <input type='hidden' name='model_num' id='model_num' value='$result[model_num]'>
+            <input type='hidden' name='product_name' id='product_name' value='$result[product_name]'>";
+        }
+
+    }
+
+?>
+
+<!-- 移動情報の記入 -->
+
+<div class="register">
+        <fieldset>
+            <legend>移動情報<br>
+                <table>
+                    <tr>
+                        <th>商品ID</th>
+                        <th>商品名</th>
+                    </tr>
+                    <?=$table?>
+                </table>
+                <?=$info?>
+            </legend>
+        </fieldset>
+</div>
+
+<form action="08-4. move product confirm.php" method="post">
+    格納経路：
+    <select name="place_from" id="place_from">
+        <option value="selected" selected>格納経路を選択</option>
+        <option value="倉庫">倉庫⇨店舗</option>
+        <option value="店舗">店舗⇨倉庫</option>
+    </select><br>
+    <div id="place_to">
+        
+    </div>
+    個数：<input type="text" name="move_amount" id="move_amount"><br>
+    担当者：<input type="text" name="person_in_charge" id="person_in_charge"><br>
+    <?=$sent?>
+    <input type="submit" value="送信" id="submit">
+</form>
+
+
+
+<script>
+
+    $("#place_from").change(function() {
+        switch($(this).val()){
+                case "倉庫":
+                    $("#place_to").empty();
+                    $("#place_to").append(`<input type="hidden" name="place_to" value="店舗">`);
+                    break;
+                case "店舗":
+                    $("#place_to").empty();
+                    $("#place_to").append(`<input type="hidden" name="place_to" value="倉庫">`);
+                    break;
+            }
+    })
+    
+    $("#submit").on("click", function(){
+        // console.log(shop);
+    // <!-- 登録ボタン押下時にどれか1つでも記入されていない場合は「全ての項目を記入してください」とアラートを出す -->
+        if($("#place_from").val()=="selected" || $("#move_amount").val()=="" || $("#person_in_charge").val()==""){
+            alert("全ての項目を記入・登録してください");
+            return false;
+        }
+    // <!-- 登録ボタン押下時に使用個数に数字が入力されていない場合は、「使用個数には数字を入力してください」とアラートを出す -->
+        else if(isNaN($("#move_amount").val())){
+            alert("使用個数には数字を入力してください");
+            return false;
+        }
+    })
+
+</script>
+
+</body>
+</html>
